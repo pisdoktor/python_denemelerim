67ba60f2c06c0723850f393a7faa43ce1e0e8e6e
Soner Ekici <sonerekici@gmail.com>
2022-03-18 15:28:50 +0300

    Denemeler devam

---
+++ fordongusu.py
@@ -0,0 +1,13 @@
+
+#meyveler dizisi
+meyveler = ["elma", "armut", "muz", "kivi", "portakal"];
+
+#for döngüsü
+for i in meyveler:
+    print(i)
+
+for k in meyveler:
+    if k == "elma":
+        print("ELMA")    
+    else:
+        break
---
+++ while.py
@@ -0,0 +1,8 @@
+# i değerini verelim
+i = 0
+
+#döngüye sokalım 5 e kadar yazdıralım
+while i < 5:
+    print(i)
+    i = i + 1;
+    
